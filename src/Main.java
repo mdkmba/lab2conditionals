fc6b878f58f44125309cda7f75c0ddecd6722b77
Marc King <mdkmba@gmail.com>
2017-07-12 15:31:42 -0400

    Fixed code

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,12 @@ public class Main
 
 
         String name;
-
         int loop1 = 0;
+        String fin;
         int loop2 = 0;
         int hereB4 = 0;
-        String fin = "No";
+
+
 
         System.out.println("This is Lab 2.");
         System.out.print("I need your name to continue.\n Enter it here: ");
@@ -23,11 +24,15 @@ public class Main
 
         do
        {
-            int num;
+            int num = 1;
             int pNum;
-            if (hereB4 == 1)
+
+
+
+
+           if (hereB4 == 1)
             {
-                System.out.printf("Hello again, %s. Please enter another positive whole number between 1 and 100: \n");
+                System.out.printf("Hello again, %s. Please enter another positive whole number between 1 and 100: \n", name);
             }
             else
             {
@@ -55,7 +60,8 @@ public class Main
             System.out.printf("Alrighty then! %s is a good number to start with, %s!\n", pNum, name);
 /*            loop1 = 1;
             System.out.println("Good so far!");
-*/
+
+*/          num = pNum;
             if (num % 2 == 1 && num > 60)
             {
                 System.out.println("Odd and over 60.");
@@ -68,32 +74,26 @@ public class Main
             {
                 System.out.println("Even and less than 25.");
             }
-            else if (num == 25)
-            {
-                System.out.print("Error in instruction logic. The number 25 is both included in specified range but not less than 25!\n Did you mean \"Even and less than 26\"?\nEnter Y or N: (");
-                String chk26 = scnr.nextLine();
-                chk26 = chk26.toUpperCase();
-                if (chk26.equals("Y"))
-                {
-                    System.out.println("Ok, cool. Even and less than 26.");
-                }
-                else if (chk26.equals("N"))
-                {
-                    System.out.println("By your command.\n\n Even and less than 25.");
-                }
-                else
-                {
-
-                }
-            }
             else if (num % 2 == 0 && num >=26 && num <= 60)
             {
                 System.out.println("Even.");
             }
+            else if (num % 2 == 0 && num > 60)
+            {
+                System.out.println("Even.");
+            }
+
+            System.out.printf("Would you like to continue, %s? (Type Yes or No)\n", name);
+
+            fin = scnr.next();
+            fin = fin.toLowerCase();
+
             if (fin.equals("no") || fin.equals("n"))
             {
                 loop1 = 1;
+               // hereB4 = 0;
             }
+                hereB4 = 1;
         }while (loop1 == 0);
     }
 }
